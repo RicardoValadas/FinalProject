349f71b510f1bf8aa83492b37b08a31b058a5892
Dio Martins <85164076+DioDev1@users.noreply.github.com>
2021-08-26 15:06:58 +0200

    Profile page

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Profile;
+use Illuminate\Support\Facades\Validator;
+
+
+class UserController extends Controller
+{
+    public function index()
+    {return view('user_profile');
+    }
+    
+}
