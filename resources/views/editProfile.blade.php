fb46d10b6c1bfaf622ed2832620d3dcfbfbd2b71
Wilalve <68129705+Wilalve@users.noreply.github.com>
2021-08-27 13:55:33 +0200

    Update editProfile.blade.php

--- resources/views/editProfile.blade.php
+++ resources/views/editProfile.blade.php
@@ -4,9 +4,9 @@
     <h1>home</h1>
     <form action=""method="post">
     @csrf
-    <input type="text" name="first_name" placeholder="first name" value="{{ old('first_name'}}" ><br>
-    <input type="text" name="last_name" placeholder="last name"value="{{ old('last_name'}}"><br>
-    <input type="text" name="email" placeholder="email"value="{{ old('name'}}"><br>
+    <input type="text" name="first_name" placeholder="first name" value="{{ old('first_name')}}"><br>
+    <input type="text" name="last_name" placeholder="last name"value="{{ old('last_name')}}"><br>
+    <input type="text" name="email" placeholder="email"value="{{ old('name')}}"><br>
     <input type="text" name="password" placeholder="password"><br>
     <input type="submit">
     </form>
