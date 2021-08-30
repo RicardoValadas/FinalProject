5e3a7eb4b232aa6bb67ba5beb35446b9d4622e50
RicardoValadas <ricardo.valadas07@gmail.com>
2021-08-30 15:02:37 +0200

    template and nav first layout finished

--- public/js/mainjs/main.js
+++ public/js/mainjs/main.js
@@ -1,12 +1,19 @@
 
-//DropDown button
+//DropDown button desktop/nav
 let dropButton = document.querySelector('.dropButton')
 dropButton.addEventListener('click', function(){
     let hidden = document.querySelector('.dropHiddenTrue')
-    
+    console.log('hi')
     if (hidden.classList.contains('dropHiddenTrue')) {
         hidden.classList.toggle('dropHiddenFalse')
     } 
-    
- 
+})
+
+//DropDown mobile button 
+let dropButtonRes = document.querySelector('.dropButtonRes')
+dropButtonRes.addEventListener('click', function(){
+    let hidden = document.querySelector('.contentDropdownLayoutRes')
+    if (hidden.classList.contains('contentDropdownLayoutRes')) {
+        hidden.classList.toggle('contentDropdownLayoutResShow')
+    }
 })
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,13 +1,11 @@
 <nav x-data="{ open: false }">
     <!-- Primary Navigation Menu -->
-
     <header>
-
         <nav>
             {{-- Logo --}}
-            <a href="{{ route('home') }}"><img src="{{ asset('images/logo.png') }}" alt=""></a>
-            <ul>
-                {{-- Nav Links --}}
+            <a href="{{ route('home') }}"><img class="logo" src="{{ asset('images/logo.png') }}" alt=""></a>
+            <ul class="desktop">
+                {{-- Nav Links desktop --}}
                 <li><a href="{{ route('home') }}">Home</a></li>
                 <li><a href="{{ route('about') }}">About</a></li>
                 <li><a href="{{ route('contact') }}">Contact</a></li>
@@ -37,41 +35,30 @@
             </div>
         </div>
 
-        <!-- Hamburger -->
-        <div class="-mr-2 flex items-center sm:hidden">
-            <button @click="open = ! open"
-                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                    <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round"
-                        stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                    <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
-                        stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                </svg>
+        <div class="username">{{ Auth::user()->username }}</div>
+        <!-- Responsive Navigation Menu -->
+        <div class="dropdownRes">
+
+            <button class="dropButtonRes">
+                <i id="menu" class="fas fa-bars"></i>
             </button>
+            <nav class="contentDropdownLayoutRes">
+
+                <div class="contentDropdownLinksRes">
+                    <a href="{{ route('home') }}">Home</a>
+                    <a href="{{ route('about') }}">About</a>
+                    <a href="{{ route('contact') }}">Contact</a>
+                    <a href="{{ route('quiz') }}">Quiz</a>
+                    <a href="{{ route('profile') }}">Profile</a>
+
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </a>
+                    </form>
+                </div>
+            </nav>
         </div>
     </header>
-
-
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div>{{ Auth::user()->username }}</div>
-            <div class="mt-3 space-y-1">
-                <!-- Authentication -->
-                <x-dropdown-link :href="route('profile')">
-                    {{ __('Profile') }}
-                </x-dropdown-link>
-
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
-            </div>
-        </div>
-    </div>
-</nav>
