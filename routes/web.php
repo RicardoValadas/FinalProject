29d7ff4cd52d73d2b932e8de0d780ff816c0aaf7
Wilalve <68129705+Wilalve@users.noreply.github.com>
2021-08-30 15:06:28 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -49,9 +49,9 @@ Route::get('/deleteProfile/delete/{id}', [UserController::class, 'destroy'])->na
 
 ###########  Route to edit the user profile (user_ajax.form.blade.php in Views )  ###########
 # Display the form
-Route::get('/ajax-form', [UserController::class, 'ajaxForm'])->name('show.ajax.form');
+Route::get('/ajax-form', [UserController::class,  'ajaxForm'])->name('show.ajax.form');
 # When we submit the form
-Route::post('/ajax-answer', [UserController::class, 'ajaxAnswer'])->name('/editProfile/{id}submit.ajax.form');
+Route::post('/ajax-answer', [UserController::class, 'ajaxAnswer'])->name('submit.ajax.form');
 
 });# End of the middleware "auth" group function
 
