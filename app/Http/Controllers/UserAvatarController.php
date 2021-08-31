bdb6b4db1575d02268b6b9162d0087297b2e8d7a
Wilalve <68129705+Wilalve@users.noreply.github.com>
2021-08-31 11:42:24 +0200

    avat test

--- app/Http/Controllers/UserAvatarController.php
+++ app/Http/Controllers/UserAvatarController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\RegisteredUserController;
 
 class UserAvatarController extends Controller
 {
-    //
+    /**
+     * Update the avatar for the user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $name = $request->file('avatar')->getClientOriginalName();
+
+        return $name;
+    }
 }
