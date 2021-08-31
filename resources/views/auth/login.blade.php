126f8a359728b6032651bf5c813367e009dea3f5
RicardoValadas <ricardo.valadas07@gmail.com>
2021-08-31 11:38:26 +0200

    changed more views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,6 @@
-<x-guest-layout>
+@extends('layouts.template')
+
+@section('content')
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -43,8 +45,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900"
-                        href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
@@ -55,4 +56,4 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+@endsection
