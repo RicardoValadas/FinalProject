1d17cb1fa5fa49ade4b05257ab61d26aa9065627
Wilalve <68129705+Wilalve@users.noreply.github.com>
2021-09-03 14:40:30 +0200

    hp

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,24 +5,27 @@
 @endsection
 
 @section('content')
-    <h1>Safefornet</h1><br>
+
+<h1>Safefornet</h1><br>
 
     <h2 class="heading-secondary">
-        Is where you learn how to be safe on the web
+        Is where you learn how to protect your kids on the web
     </h2><br>
+    <!--    ***  first image  ***    -->
     <section id=imghead>
-        <a href="/">
-            <img id="imghome1" src="{{ asset('home_images/kidandparents.jpg') }}" alt="">
-        </a>
-    </section>
-    <section id="firstSec">
-    <div class="parent">
+        <img id="imghome1" src="{{ asset('home_images/kidandparents.jpg') }}" alt="">
+    </section><br>
+
+ <!--    ***  First Section   ***    -->
+<section id="firstSec">
+    <div class="container">
         <div class="head2">
             <h3 class="heading-tertiary2">How to protect young kids from inappropriate Internet</h3><br>
             <p class="paragraph1">
                 Setting strict rules for your young children's Internet adventures protects them from inappropriate content, cyberbullies, identity thieves and child predators. Installing popup blockers and reminding your little one to never, ever give out her real name, age, or address online is a solid start.
             </p>
         </div>
+
         <div class="head3">
             <h3 class="heading-tertiary3">Prevention of phishing attack</h3><br>
             <p class="paragraph2">
@@ -30,18 +33,24 @@
             </p>
         </div>
     </div>
-</section>
+</section><br><br>
 
+<!-- Section with the second image -->
+<section id=imghead>
+    <img id="imghome2" src="{{ asset('home_images/max-bender-XIVDN9cxOVc-unsplash.jpg') }}" alt="">
+</section><br><br>
+
+
+    <div class="head4">
+        <h3 class="heading-tertiary4">Teach them to keep private info private</h3><br>
+        <p class="paragraph3">
+            Always insist that your kids never reveal their real names (first, last, or imaginary) or where they live, go to school, hang out or play. The same rule goes for any other personal and confidential information. Repeat this rule often until they say it in their sleep, even if they kick and scream and roll their eyes. While you're at it, tell them not to share your personal information or their siblings', either.
+        </p>
+    </div>
+
+<!--   Second section  with -->
 <section id="secondSec">
     <div class="parent1">
-        <div class="head4">
-            <h3 class="heading-tertiary4">Teach them to keep private info private</h3><br>
-            <p class="paragraph3">
-                Always insist that your kids never reveal their real names (first, last, or imaginary) or where they live, go to school, hang out or play. The same rule goes for any other personal and confidential information. Repeat this rule often until they say it in their sleep, even if they kick and scream and roll their eyes. While you're at it, tell them not to share your personal information or their siblings', either.
-            </p>
-        </div>
-    </div>
-    <div class="parent2">
         <div class="head5">
             <h3 class="heading-tertiary5">Take advantage of your browser's parental controls</h3><br>
             <p class="paragraph4">
@@ -54,6 +63,10 @@
                 The majority of Internet browsers (including Mozilla Firefox, Google Chrome, Safari and many others) have an Internet options folder where you can easily set up security safeguards and content filters for language, nudity, sex, and violence. Or skip the filter fiddling and only allow your kids to use squeaky clean browsers created just for kids.
             </p>
         </div>
+
+    </div>
+    <div class="parent2">
+
         <div class="head7">
             <h3 class="heading-tertiary7">Take advantage of your browser's parental controls</h3><br>
             <p class="paragraph6">
@@ -66,13 +79,7 @@
                 The majority of Internet browsers (including Mozilla Firefox, Google Chrome, Safari and many others) have an Internet options folder where you can easily set up security safeguards and content filters for language, nudity, sex, and violence. Or skip the filter fiddling and only allow your kids to use squeaky clean browsers created just for kids.
             </p>
         </div>
-        <div class="head9">
-            <h3 class="heading-tertiary9">Take advantage of your browser's parental controls</h3><br>
-            <p class="paragraph8">
-                The majority of Internet browsers (including Mozilla Firefox, Google Chrome, Safari and many others) have an Internet options folder where you can easily set up security safeguards and content filters for language, nudity, sex, and violence. Or skip the filter fiddling and only allow your kids to use squeaky clean browsers created just for kids.
-            </p>
-        </div>
-        
+
 </section>
 
 @endsection
