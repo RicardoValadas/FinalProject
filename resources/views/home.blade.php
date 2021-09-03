e9e0de623249fd011deb55b1cc6481387f66afee
Wilalve <68129705+Wilalve@users.noreply.github.com>
2021-09-03 16:09:24 +0200

    VIP
    home,  middleware, routes, migrations

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,21 +12,21 @@
         Is where you learn how to protect your kids on the web
     </h2><br>
     <!--    ***  first image  ***    -->
-    <section id=imghead>
-        <img id="imghome1" src="{{ asset('home_images/kidandparents.jpg') }}" alt="">
+    <section id="imghead">
+        {{-- <img id="imghome1" src="{{ asset('home_images/kidandparents.jpg') }}" alt=""> --}}
     </section><br>
 
  <!--    ***  First Section   ***    -->
 <section id="firstSec">
-    <div class="container">
-        <div class="head2">
+    <div class="row cont1">
+        <div class="col head2">
             <h3 class="heading-tertiary2">How to protect young kids from inappropriate Internet</h3><br>
             <p class="paragraph1">
                 Setting strict rules for your young children's Internet adventures protects them from inappropriate content, cyberbullies, identity thieves and child predators. Installing popup blockers and reminding your little one to never, ever give out her real name, age, or address online is a solid start.
             </p>
         </div>
 
-        <div class="head3">
+        <div class="col head3">
             <h3 class="heading-tertiary3">Prevention of phishing attack</h3><br>
             <p class="paragraph2">
                 Use anti-phishing protection and anti-spam software to protect yourself when malicious messages slip through to your computer. Anti-malware is included to prevent other types of threats. Similar to anti-spam software, anti-malware software is programmed by security researchers to spot even the stealthiest malware.
@@ -35,11 +35,16 @@
     </div>
 </section><br><br>
 
+<!--
+<div class="container">
+    <div class="row">
+        <div class="col">col1</div>
+        <div class="col">col2</div>
+        <div class="col">col3</div>
+    </div>
+</div>
+-->
 <!-- Section with the second image -->
-<section id=imghead>
-    <img id="imghome2" src="{{ asset('home_images/max-bender-XIVDN9cxOVc-unsplash.jpg') }}" alt="">
-</section><br><br>
-
 
     <div class="head4">
         <h3 class="heading-tertiary4">Teach them to keep private info private</h3><br>
@@ -65,6 +70,10 @@
         </div>
 
     </div>
+    
+    <div class=imghead>
+        <img id="imghome2" src="{{ asset('home_images/max-bender-XIVDN9cxOVc-unsplash.jpg') }}" alt="">
+    </div><br><br>
     <div class="parent2">
 
         <div class="head7">
@@ -79,7 +88,7 @@
                 The majority of Internet browsers (including Mozilla Firefox, Google Chrome, Safari and many others) have an Internet options folder where you can easily set up security safeguards and content filters for language, nudity, sex, and violence. Or skip the filter fiddling and only allow your kids to use squeaky clean browsers created just for kids.
             </p>
         </div>
-
 </section>
+
 
 @endsection
