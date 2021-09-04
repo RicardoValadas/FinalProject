4023a5996608f6e665c0b85354db75d60da096d7
Wilalve <68129705+Wilalve@users.noreply.github.com>
2021-09-04 16:56:12 +0200

    vip
    vip home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
     <!--    ***  first image  ***    -->
     <section id="imghead">
         {{-- <img id="imghome1" src="{{ asset('home_images/kidandparents.jpg') }}" alt=""> --}}
-    </section>
+    </section><br>
 
  <!--    ***  First Section   ***    -->
 <section id="firstSec">
@@ -57,7 +57,7 @@
 
     <div class="col head4">
         <article id="art3">
-            <h3 class="heading-tertiary4">Teach them to keep private their personal info </h3><br>
+            <h3 class="heading-tertiary4">Teach them to keep private their personal informations</h3><br>
             <img id="imgart3" src="{{ asset('home_images/kevin-ku-w7ZyuGYNpRQ-unsplash.jpg') }}" alt=""><br><br>
                 <p class="paragraph3">
                     Always insist that your kids never reveal their real names (first, last, or imaginary) or where they live, go to school, hang out or play. The same rule goes for any other personal and confidential information. Repeat this rule often until they say it in their sleep, even if they kick and scream and roll their eyes. While you're at it, tell them not to share your personal information or their siblings', either.
@@ -101,11 +101,12 @@
     </div><br><br><br>
     <section id="imghome2">
         {{-- <img id="imghome1" src="{{ asset('home_images/kidandparents.jpg') }}" alt=""> --}}
-    </section>
+    </section><br><br>
     <div class="row cont3">
 
         <div class="col head7">
             <h3 class="heading-tertiary7">Talk openly with your child about their online activity </h3><br>
+            <img id="imgart6" src="{{ asset('home_images/thisisengineering-raeng-8hgmG03spF4-unsplash.jpg') }}" alt=""><br><br>
             <p class="paragraph6">
                 As soon as your child starts accessing the internet, talk to them about what they are reading, watching and who they are communicating with online – and keep the conversation going as they grow older. Ask your child what sites they visit or apps they use, write a list, and look at them together. Talk to your child about what you think is appropriate, and remind them that this may be different for other parents and their children.
             </p><br>
@@ -118,6 +119,8 @@
         </div>
         <div class="col head8">
             <h3 class="heading-tertiary8">Keep control of your family’s digital footprint </h3><br>
+            <img id="imgart7" src="{{ asset('home_images/petter-lagson-VH_L_H4w7U8-unsplash.jpg') }}" alt=""><br><br>
+
             <p class="paragraph7">
                 Every picture and personal detail that is posted and shared on social media and the internet contributes to someone’s digital footprint. The big risk with this is that once information is shared publicly, it can be used in ways you may not expect and cannot control. You should also assume that anything that is put online is permanent (it can sometimes be deleted but not always before others have seen it and saved it). For this reason, children and young people need to be smart about protecting their images and information. The same goes for parents who regularly post pictures of their children’s online.
             </p><br>
@@ -126,6 +129,4 @@
             </p><br>
         </div>
 </section>
-
-
 @endsection
