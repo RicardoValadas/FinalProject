49ef86c3fc037ab8dc44ecce2619863268f3c840
RicardoValadas <ricardo.valadas07@gmail.com>
2021-09-06 15:57:16 +0200

    hi

--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -3,7 +3,7 @@
 @if ($errors->any())
     <div {{ $attributes }}>
         <div class="font-medium text-red-600">
-            {{ __('Error Bitch try again') }}
+            {{ __('Failed') }}
         </div>
 
         <ul class="mt-3 list-disc list-inside text-sm text-red-600">
