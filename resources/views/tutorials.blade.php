824dc17e3fd6842ecf854c7a2c5be419d47543ef
Dio Martins <85164076+DioDev1@users.noreply.github.com>
2021-09-06 11:56:20 +0200

    tutorial

--- resources/views/tutorials.blade.php
+++ resources/views/tutorials.blade.php
@@ -5,13 +5,50 @@
 
 @section('content')
 <div>
-    <section>
-       <h1>How to install a antivirus?</h1>
-       
-       <img class="contactLogo" src="{{ asset('images/antovirus.jpg') }}" alt="">
-       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ex assumenda unde! Possimus vel quibusdam sequi velit pariatur dolores, ipsum quod ducimus totam nam, labore repellat itaque delectus recusandae nihil!</p>
+    <h1><strong>What is an antivirus and how does it work?</strong></h1>
+    <iframe src="https://www.youtube.com/embed/jW626WMWNAE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <a href="https://www.youtube.com/embed/jW626WMWNAE" target='blank'>What is an antivirus and how does it work?</a>
+    <p>Author : <a href="https://www.youtube.com/channel/UC0jJMyy2fu1mk2McRcEqfRA" target='blank'>SafetyDetectives</a></p>
+</div>
 
-    </section>
+<div>
+    <h1><strong>Block all adult sites with single setting change for your kid</strong></h1>
+    <iframe src="https://www.youtube.com/embed/pnySloWemoE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <a href="https://www.youtube.com/watch?v=pnySloWemoE" target='blank'>What is an antivirus and how does it work?</a>
+    <p>Author : <a href="https://www.youtube.com/channel/UC-vvwzJHaAxgTkklsycfsXg" target='blank'>
+The Geek Page</a></p>
+</div>
+
+<div>
+    <h1><strong>5 Ways to Make YouTube Safer for Kids</strong></h1>
+    <iframe src="https://www.youtube.com/embed/N8P_6aj8VbU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <a href="https://www.youtube.com/embed/N8P_6aj8VbU" target='blank'>What is an antivirus and how does it work?</a>
+    <p>Author : <a href="https://www.youtube.com/channel/UCYJTfZSjqUraGJu_GwCwHpw" target='blank'>
+Common Sense Media Ratings & Reviews</a></p>
+</div>
+
+<div>
+    <h1><strong>5 Tips to Keep Your Child Safe On The Internet</strong></h1>
+    <iframe  src="https://www.youtube.com/embed/NqlTOOY9CIo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <a href="https://www.youtube.com/embed/NqlTOOY9CIo" target='blank'>What is an antivirus and how does it work?</a>
+    <p>Author : <a href="https://www.youtube.com/channel/UC6KRRypaw5MfPmi0ImcJY-A" target='blank'>
+Hampton Primary School</a></p>
+</div>
+
+<div>
+    <h1><strong>What Is Phishing and How to Avoid the Bait</strong></h1>
+    <iframe  src="https://www.youtube.com/embed/XsOWczwRVuc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <a href="https://www.youtube.com/embed/XsOWczwRVuc" target='blank'>What is an antivirus and how does it work?</a>
+    <p>Author : <a href="https://www.youtube.com/channel/UC0jJMyy2fu1mk2McRcEqfRA" target='blank'>
+iluli by Mike Lamb</a></p>
+</div>
+
+<div>
+    <h1><strong>Four Reasons to Care About Your Digital Footprint</strong></h1>
+    <iframe  src="https://www.youtube.com/embed/Ro_LlRg8rGg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <a href="https://www.youtube.com/embed/Ro_LlRg8rGg" target='blank'>What is an antivirus and how does it work?</a>
+    <p>Author : <a href="https://www.youtube.com/channel/UCmOv9ijvfjHcJdcuqyyyamg" target='blank'>
+Internet Society</a></p>
 </div>
 
 @endsection
