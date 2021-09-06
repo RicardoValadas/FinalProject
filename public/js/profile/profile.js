18b1a2ab6050a101ad1fbbe497ab45f39a880e1a
RicardoValadas <ricardo.valadas07@gmail.com>
2021-09-06 22:50:29 +0200

    fix in profile js

--- public/js/profile/profile.js
+++ public/js/profile/profile.js
@@ -1,23 +1,17 @@
-// NEED TO CHANGE
-/*let buttonAchieve = document.querySelector('#buttonAchieve')
-buttonAchieve.addEventListener('click', function(){
-    let hidden = document.querySelector('#achieveSection')
-    if (hidden.classList.contains('achieveHidden')) {
-        hidden.classList.toggle('achieveShow')
-    } 
-})*/
 
-
-let achiev=document.querySelector('#showachi');
-achiev.addEventListener('click', function(){
+const achiev=document.querySelector('#showachi');
+    achiev.addEventListener('click', function(){
     document.querySelector('.achievments').classList.toggle('visible'); 
-    
-    let score= document.querySelector('.userscore').value;
-
+    //gets the score from the hidden input
+    const score = document.querySelector('#scoreValue').value
+    //checks the score
+    console.log(score)
+    //change here for the medal system
     if(score>=1){
     document.querySelector('participation').style.display= "none";
     console.log("hello");
     }
     
 });
+
 
