1bb0e9c8d8b2ae9eaf82c7f03bbe77c45d26a165
Gabriel Silva <85163916+gabrielsilva14@users.noreply.github.com>
2021-09-07 09:05:18 +0200

    achievements finished

--- public/js/profile/profile.js
+++ public/js/profile/profile.js
@@ -5,11 +5,28 @@ const achiev=document.querySelector('#showachi');
     //gets the score from the hidden input
     const score = document.querySelector('#scoreValue').value
     //checks the score
-    console.log(score)
     //change here for the medal system
+
+    //particpation
     if(score>=1){
-    document.querySelector('participation').style.display= "none";
-    console.log("hello");
+    document.querySelector('#participation').style.display= "block";
+   
+    //bronze
+    }
+    if(score>=10){
+    document.querySelector('#bronze').style.display= "block";
+   
+
+    //silver
+    }
+    if(score>=30){
+    document.querySelector('#silver').style.display= "block";
+   
+    //gold
+    }
+    if(score>=50){
+    document.querySelector('#gold').style.display= "block";
+   
     }
     
 });
